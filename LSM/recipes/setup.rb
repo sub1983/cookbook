3b14801c85f58709686f949c62282a8955c360e2
subinmath <subin.mathew@crichq.com>
2016-08-24 14:18:51 +0530

    tar

--- LSM/recipes/setup.rb
+++ LSM/recipes/setup.rb
@@ -1,6 +1,6 @@
 remote_file '/tmp/lsm.tar.gz' do
   source 'http://www.rfxn.com/downloads/lsm-current.tar.gz'
-  tarball_x '/tmp/lsm.tar.gz' 
+  tar_package '/tmp/lsm.tar.gz' 
   destination '/opt/test'
   action  :extract
 end
