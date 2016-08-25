ae3b407d2f89cb3c8f0fd1fad6af00931c6201a8
subinmath <subin.mathew@crichq.com>
2016-08-25 11:11:54 +0530

    Update setup.rb

--- LSM/recipes/setup.rb
+++ LSM/recipes/setup.rb
@@ -1,11 +1,13 @@
 remote_file '/tmp/lsm.tar.gz' do
   source 'http://www.rfxn.com/downloads/lsm-current.tar.gz'
 end    
-  execute 'untar_file' do
+
+execute 'untar_file' do
   cwd '/tmp/'
   command 'tar -zxvf lsm.tar.gz'
-  end
-  execute 'install lsm' do 
+end
+
+execute "install lsm #{node[lsm][version]}" do 
   cwd '/tmp'
-  command 'sh /tmp/lsm-#{[lsm][version]}/install.sh '
-  end
+  command "sh /tmp/lsm-#{node[lsm][version]}/install.sh "
+end
