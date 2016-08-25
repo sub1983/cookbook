9bad379d51c0dc95896d4d93c81129e6a27708c0
subinmath <subin.mathew@crichq.com>
2016-08-25 14:21:21 +0530

    tomcat

---
+++ tomcat/recipes/setup.rb
@@ -0,0 +1,13 @@
+remote_file "/tmp/tomcat#{node['tomcat']['version']}.tar.gz" do
+  source 'http://mirror.fibergrid.in/apache/tomcat/tomcat-8/v8.5.4/bin/apache-tomcat-8.5.4.tar.gz'
+end    
+
+execute 'untar_file' do
+  cwd '/tmp/'
+  command "tar -zxvf #{node['tomcat']['version']}.tar.gz"
+end
+
+#execute " install lsm-#{node['lsm']['version']} " do 
+#  cwd '/tmp'
+#  command "sh /tmp/lsm-#{node['lsm']['version']}/install.sh "
+#end
