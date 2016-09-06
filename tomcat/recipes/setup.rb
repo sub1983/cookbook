9cf08d5d119795974265e5abf9d98de6373618e0
subinmath <subin.mathew@crichq.com>
2016-09-06 14:38:23 +0530

    Update setup.rb

--- tomcat/recipes/setup.rb
+++ tomcat/recipes/setup.rb
@@ -1,5 +1,5 @@
 remote_file "/tmp/apache-tomcat-#{node['tomcat']['version']}.tar.gz" do
-  source 'http://mirror.fibergrid.in/apache/tomcat/tomcat-8/v8.5.4/bin/apache-tomcat-8.5.4.tar.gz'
+  source 'http://mirror.fibergrid.in/apache/tomcat/tomcat-8/v8.5.4/bin/#{node['tomcat']['version']}.tar.gz'
 end    
 
 execute 'untar_file' do
